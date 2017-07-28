b2a5ade8cb1fe39864dd531b737dc84fd5bd0731
Manabu Ori <manabu.ori@gmail.com>
2017-07-28 14:26:42 +0900

    first commit.

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+print "Hello, World... from " . $_SERVER['SERVER_ADDR'] . " to " . $_SERVER['REMOTE_ADDR'] . ".\n";
+/*
+print "PHP version: " . PHP_VERSION . "\n";
+print "IP address: " . $_SERVER['SERVER_ADDR'] . "\n";
+print "Server name: " . $_SERVER['SERVER_NAME'] . "\n";
+*/
+?>
