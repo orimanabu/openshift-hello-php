4c341d3541c1b6631e78c1c51060b3f4510cbf28
Manabu Ori <manabu.ori@gmail.com>
2021-05-09 22:31:35 +0900

    Remove some comments

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
 print "Hello World from PHP Pod: SERVER_ADDR=" . $_SERVER['SERVER_ADDR'] . ", REMOTE_ADDR=" . $_SERVER['REMOTE_ADDR'] . ".\n";
-/*
-print "PHP version: " . PHP_VERSION . "\n";
-print "IP address: " . $_SERVER['SERVER_ADDR'] . "\n";
-print "Server name: " . $_SERVER['SERVER_NAME'] . "\n";
-*/
 ?>
