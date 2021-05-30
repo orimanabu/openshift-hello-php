a1f97890ff9340ced50ac1333780aac2a6f09e04
Manabu Ori <manabu.ori@gmail.com>
2021-05-30 16:56:30 +0900

    add X-Forwarded-For.

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello World from PHP Pod: SERVER_ADDR=" . $_SERVER['SERVER_ADDR'] . ", REMOTE_ADDR=" . $_SERVER['REMOTE_ADDR'] . ".\n";
+print "Hello World from PHP Pod: SERVER_ADDR=" . $_SERVER['SERVER_ADDR'] . ", REMOTE_ADDR=" . $_SERVER['REMOTE_ADDR'] . ", X-Forwarded-For=" . $_SERVER['HTTP_X_FORWARDED_FOR'] . ".\n";
 ?>
